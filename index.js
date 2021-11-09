eb31d7913fce3a6e78f0936a9e17c6dc0404661f
TurtlePaw <turtlesimsyt@gmail.com>
2021-11-08 21:39:49 -0500

    Add context menus.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const Discord = require('discord.js');
 const config = require('./config.json');
 const fs = require('fs');
 const { prefix } = require('./config.json');
+const du = require('discord.js-util');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
 const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_PRESENCES", "GUILD_INTEGRATIONS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGES", "DIRECT_MESSAGE_REACTIONS"],
@@ -13,8 +16,11 @@ client.on('ready', () => {
 
 client.commands = new Discord.Collection();
 client.scommands = new Discord.Collection();
+client.contextMenus = new Discord.Collection();
 client.config = config;
 client.color = config.color;
+
+const rest = new REST({ version: '9' }).setToken(config.token);
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -24,6 +30,44 @@ for (const file of commandFiles) {
 	// with the key as the command name and the value as the exported module
 	client.commands.set(command.name, command);
 }
+
+const menus = {
+	out: [
+
+	],
+	not_out: [
+		new du.ContextMenuBuilder()
+		.setName("Avatar")
+		.setType("USER")
+	]
+}
+const menusJSON = [];
+const ContextCommandFiles = fs.readdirSync('./Context_Menus').filter(file => file.endsWith('.js'));
+
+for (const file of ContextCommandFiles) {
+	const command = require(`./Context_Menus/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.contextMenus.set(command.name, command);
+}
+for(const menu of menus.not_out){
+	menusJSON.push(menu.toJSON());
+}
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands("869396819615027240", "868921019073708092"),
+			{ body: menusJSON },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
 
 const commands = [];
 const scommandFiles = fs.readdirSync('./slash_commands').filter(file => file.endsWith('.js'));
@@ -35,6 +79,18 @@ for (const file of scommandFiles) {
 	client.scommands.set(command.name, command);
 	commands.push(command)
 }
+
+client.on("interactionCreate", async interaction => {
+	if(!interaction.isContextMenu()) return
+
+	const menu = client.contextMenus.get(interaction.commandName);
+
+	try {
+		await menu.execute(client, interaction);
+	} catch(e){
+		console.log(e);
+	}
+})
 
 client.on('messageCreate', async message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
